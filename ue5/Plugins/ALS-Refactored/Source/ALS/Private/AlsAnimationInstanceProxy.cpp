77d5ed41c60fb84b5aeb7daac8a326e17dea1cd9
edi3on <ed@sciencevr.com>
2025-02-09 00:03:54 -0800

    added als version + added connections to eliza and local servers

--- ue5/Plugins/ALS-Refactored/Source/ALS/Private/AlsAnimationInstanceProxy.cpp
+++
@@ -1,5 +0,0 @@
-#include "AlsAnimationInstanceProxy.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(AlsAnimationInstanceProxy)
-
-FAlsAnimationInstanceProxy::FAlsAnimationInstanceProxy(UAnimInstance* AnimationInstance): FAnimInstanceProxy{AnimationInstance} {}
